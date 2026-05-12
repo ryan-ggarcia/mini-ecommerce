4679ce11145d8227ae2eba3ae4105aab47397027
Ryan Fidelis <ryanmr.fidelis@gmail.com>
2026-05-12 16:58:41 -0300

    Colocando fecth API na aplicação e validação de formuilario

--- Controller/UsuarioController.js
+++ Controller/UsuarioController.js
@@ -1,11 +1,14 @@
 const UsuarioModel = require('../Models/UsuarioModel')
-
+const EnderecoModel = require('../Models/EnderecoModel')
 class UsuarioController{
     async readUser(req,res){
         res.render('admin/usuario/listar')
     }
     registerView(req,res){
         res.render('admin/usuario/cadastrar')
+    }
+    async newRegister(req,res){
+
     }
 }
 
