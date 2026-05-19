9bf96b35558471b4b134d9846c030d3077f5048f
Ryan Fidelis <ryanmr.fidelis@gmail.com>
2026-05-19 16:59:45 -0300

    Integrando multer
    Configurando o sistema para suportar o envio de imagem no banco de dados

--- Controller/produtoController.js
+++ Controller/produtoController.js
@@ -12,6 +12,25 @@ class ProdutoController{
         marca = await marca.read()
         res.render('admin/produto/cadastrar',{cat,marca})
     }
-
+    async newRegister(req,res){
+        let ok = true
+        let msg = ''
+        if(req.body.nome && req.body.preco && req.body.quantidade && req.body.descricao
+            && req.body.status && req.body.categoria && req.body.marca && req.file ){
+                // console.log(req.file.filename)
+                let model = new ProdutoModel(0,req.body.nome,req.body.descricao,req.file.filename
+                    ,req.body.preco,req.body.quantidade,req.body.categoria,req.body.marca,req.body.validade,
+                    req.body.status)
+                let result = await model.create()
+                if(result){
+                    msg = 'Sucesso ao cadastrar o produto!'
+                    return res.send({msg,ok})
+                }else{
+                    ok = false
+                    msg = 'Não foi possível realizar o cadastro de produto'
+                    return res.send({msg,ok})
+                }
+        }
+    }
 }
 module.exports = ProdutoController
